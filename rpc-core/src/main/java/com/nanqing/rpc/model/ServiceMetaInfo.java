b9fb79479d07f5dd5931854cd1ec01fb332c0026
neptunejing <jingnanqing@hotmail.com>
2024-07-23 12:05:12 +0800

    feat: 注册中心开发

---
+++ rpc-core/src/main/java/com/nanqing/rpc/model/ServiceMetaInfo.java
@@ -0,0 +1,48 @@
+package com.nanqing.rpc.model;
+
+/**
+ * 服务元信息（注册信息）
+ */
+public class ServiceMetaInfo {
+    /**
+     * 服务名称
+     */
+    private String serviceName;
+
+    /**
+     * 服务版本号
+     */
+    private String serviceVersion = "1.0";
+
+    /**
+     * 服务域名
+     */
+    private String serviceHost;
+
+    /**
+     * 服务端口
+     */
+    private Integer servicePort;
+
+    /**
+     * 服务分组（暂未实现）
+     */
+    private String serviceGroup = "default";
+
+    /**
+     * 获取完整键名
+     */
+    public String getServiceKey() {
+        return String.format("%s:%s", serviceName, serviceVersion);
+        // 后续可包含分组
+        // return String.format("%s:%s:%s", serviceName, serviceVersion, serviceGroup);
+    }
+
+    /**
+     * 获取服务注册节点键名
+     */
+    public String getServiceNodeKey() {
+        return String.format("%s/%s:%s", getServiceKey(), serviceHost, serviceVersion);
+    }
+}
+
---
+++ rpc-core/src/main/java/com/nanqing/rpc/registry/Registry.java
@@ -0,0 +1,36 @@
+package com.nanqing.rpc.registry;
+
+import com.nanqing.rpc.config.RegistryConfig;
+import com.nanqing.rpc.model.ServiceMetaInfo;
+
+import java.util.List;
+
+/**
+ * 注册中心
+ */
+public interface Registry {
+    /**
+     * 初始化
+     */
+    void init(RegistryConfig registryConfig);
+
+    /**
+     * 注册服务（服务端）
+     */
+    void register(ServiceMetaInfo serviceMetaInfo) throws Exception;
+
+    /**
+     * 注销服务（服务端）
+     */
+    void unregister(ServiceMetaInfo serviceMetaInfo);
+
+    /**
+     * 服务发现（获取某个服务的所有节点，消费端）
+     */
+    List<ServiceMetaInfo> serviceDiscovery(String serviceKey);
+
+    /**
+     * 服务销毁
+     */
+    void destory();
+}
